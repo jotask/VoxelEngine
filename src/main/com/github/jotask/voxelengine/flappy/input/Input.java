73948b2b8bf921fcbcaf72ad7237fdd1abe726d3
jota_sk <jota_sk@hotmail.com>
2017-05-30 15:13:01 +0100

    created input

---
+++ src/main/com/github/jotask/voxelengine/flappy/input/Input.java
@@ -0,0 +1,21 @@
+package com.github.jotask.voxelengine.flappy.input;
+
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWKeyCallback;
+
+/**
+ * Input
+ *
+ * @author Jose Vives Iznardo
+ * @since 30/05/2017
+ */
+public class Input extends GLFWKeyCallback {
+
+    public static boolean[] keys = new boolean[65536];
+
+    @Override
+    public void invoke(long window, int key, int scancode, int action, int mods) {
+        keys[key] = action != GLFW.GLFW_RELEASE;
+    }
+
+}
